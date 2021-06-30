447e4e02822b8109b3ba38d10465fb183efb5fd2
Phoenix Dimagiba <42821744+pdimagiba@users.noreply.github.com>
2021-06-30 08:48:04 -0700

    Update keypadnumtoletter.py

--- keypadnumtoletter.py
+++ keypadnumtoletter.py
@@ -1,4 +1,4 @@
-# Given a numerical PIN, finds all possible permutations of telephone keypad letter equivalents.
+# Given a numerical PIN, finds all possible combinations of telephone keypad letter equivalents.
 # Please see the image in this repository for an example of such a keypad.
 # This program assumes that any given digit can correspond to any of its assigned letters.
 # This program does not account for multiple presses of a keypad number cycling through its assigned letters.
